2b548aafa34d61d3e8b46f3c85bbda76b915a240
robertDurst <rsdurst@colby.edu>
2019-02-08 16:49:14 -0500

    Basic speaking app that breaks with a deprecation warning

---
+++ speak.js
@@ -0,0 +1,12 @@
+const speak = words => {
+  var sys = require('util')
+  var exec = require('child_process').exec;
+  function puts(error, stdout, stderr) {
+    sys.puts(stdout)
+  }
+  exec(`say "${words}"`, puts);
+};
+
+module.exports = {
+  speak,
+}
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const BASE_URL = 'https://colbyshuttle.ridesystems.net/Services/JSONPRelay.svc';
-
+const {speak} = require('./speak');
 
 const getShuttleLocation =
     async () => {
@@ -14,8 +14,63 @@ const getShuttleRoute =
   return resp.data;
 }
 
+const getEstimates =
+    async () => {
+  const resp = await axios.get(BASE_URL + '/GetMapStopEstimates')
+  return resp.data;
+}
+
+let isArriving = false;
 const seconds = 1000;
 setInterval(async () => {
-  const resp2 = await getShuttleLocation();
-  console.log(resp2)
+  try {
+    const data = {};
+    const resp2 = await getShuttleLocation();
+
+    resp2.forEach(x => {
+      data[x.VehicleID] =
+      {
+        groundSpeed: x.GroundSpeed, routeID: x.RouteID, name: x.Name,
+            direction: x.Heading, stops: [],
+      }
+    });
+    const resp = await getEstimates();
+    resp.forEach(x => {
+      x.RouteStops.forEach(y => {
+        y.Estimates.forEach(z => {data[z.VehicleID].stops.push({
+                              name: y.Description,
+                              order: y.StopOrder,
+                              secondsUntilStop: z.SecondsToStop,
+                            })});
+      });
+    })
+
+    if (!isArriving) {
+      Object.keys(data).forEach(busID => {
+        const nextStop = data[busID].stops.sort(
+            (a, b) => a.secondsUntilStop > b.secondsUntilStop)[0];
+        if (data[busID].groundSpeed == 0 && nextStop.secondsUntilStop < 20) {
+          isArriving = true;
+          speak(`Bus id ${busID} has arrived at ${nextStop.name}.`)
+        }
+      });
+    }
+    else {
+      if (data[busID].groundSpeed > 0 && nextStop.secondsUntilStop > 20) {
+        isArriving = false;
+      }
+    }
+
+
+
+    Object.keys(data).forEach(busID => {
+      console.log('BUS ID: ' + busID + ' stops:')
+      data[busID]
+          .stops.sort((a, b) => a.secondsUntilStop > b.secondsUntilStop)
+          .forEach(x => console.log(x));
+      console.log('Speed: ' + data[busID].groundSpeed);
+      console.log();
+    });
+  } catch (error) {
+  }
 }, 5 * seconds)
