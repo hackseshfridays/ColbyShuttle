e939711cfaca7dedbf2f901886dfba92eff993d3
robertDurst <rsdurst@colby.edu>
2019-02-15 20:00:39 -0500

    begin message parsing

--- textupdater/twilio.js
+++ textupdater/twilio.js
@@ -1,1 +1,30 @@
-// code here
+const accountSid = process.env.TWILIO_SSID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+const {getShuttleLocation} = require('../api/colbyshuttle');
+const {parseMessage} = require('./message_parser');
+
+const send_text = (phone_number, message) => {
+  client.messages
+      .create({
+        body: message,
+        from: process.env.TWILIO_PHONE_NUMBER,
+        to: phone_number
+      })
+      .then(message => console.log(message.sid))
+      .done();
+};
+
+const receive_message = body => {
+  // here is where we collect data for analysis and bug reports
+  const phoneNumber = body.From;
+  const message = body.Body;
+
+  const parsedMessage = parseMessage(message);
+
+  console.log(parsedMessage);
+};
+
+module.exports = {
+  receive_message,
+}
